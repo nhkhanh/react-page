4e86bd63738d8086db6425f402d6678cc17eba4a
Khanh Nguyen <nguyenhuykhanh@gmail.com>
2020-04-04 08:48:23 +0700

    Wrap plain text paste in paragraph even with one line break

--- packages/plugins/content/slate/src/slateEnhancer/withPaste.ts
+++ packages/plugins/content/slate/src/slateEnhancer/withPaste.ts
@@ -9,12 +9,6 @@ const withPaste = (plugins: SlatePlugin[], defaultPluginType: string) => (
   const { insertData } = editor;
   const htmlToSlate = HtmlToSlate({ plugins });
   editor.insertData = data => {
-    const slateData = data.getData('application/x-slate-fragment');
-    if (slateData) {
-      insertData(data);
-      return;
-    }
-
     const html = data.getData('text/html');
     if (html) {
       const { slate } = htmlToSlate(html);
@@ -25,29 +19,13 @@ const withPaste = (plugins: SlatePlugin[], defaultPluginType: string) => (
 
     const text = data.getData('text/plain');
     if (text) {
-      // if there are two subsequent line breks, insert paragraph, otherway insert soft line break
       const lines = text.split('\n');
-      let nextWillbeParagraph = false;
-      for (let i = 0; i < lines.length; i++) {
-        const thisLine = lines[i];
-        const nextLine = lines[i + 1];
-        // add a \n, unless the next line is empty, then its either the last entry or the following wil be a paragraph
-        const nextIsEmpty = !nextLine || !nextLine.trim();
-
-        const thisLineText = thisLine + (nextIsEmpty ? '' : '\n');
-        if (!thisLine.trim()) {
-          // this line is empty,
-          nextWillbeParagraph = true;
-        } else if (nextWillbeParagraph) {
+      Transforms.insertText(editor, lines[0]);
+      for (let i = 1; i < lines.length; i++) {
           Transforms.insertNodes(editor, {
             type: defaultPluginType,
-            children: [{ text: thisLineText }],
+            children: [{ text: lines[i] }],
           });
-          nextWillbeParagraph = false;
-        } else {
-          Transforms.insertText(editor, thisLineText);
-          nextWillbeParagraph = false;
-        }
       }
       return;
     }
